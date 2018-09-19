402b78b1d75f084154bd2d61ebc552c9e53150dc
Amalesh <amalesh.debnath@gmail.com>
2018-09-20 01:41:18 +0600

    Reason: fix the ui issue in doctor page after search

--- application/views/js/frontend-doctor-script.php
+++ application/views/js/frontend-doctor-script.php
@@ -13,37 +13,35 @@
                         var doctor_info = doctorData[i];
                         var image_path = doctor_info.ImagePath == '' || doctor_info.ImagePath == null ? '<?php echo base_url();?>application/views/img/doctor.png' : '<?php echo base_url();?>application/views/img/'+doctor_info.ImagePath;
                         var hotline_info = doctor_info.Hotline == '' || doctor_info.Hotline == null ? '' : '<br>Hotline: '+doctor_info.Hotline;
-                        var doctor_li_text = '<div class="doctor-list">' +
-                            '                        <div class="doctor-photo">' +
-                            '                            <a href=""><img src="'+image_path+'" alt="doctor"></a>' +
-                            '                        </div>' +
-                            '                        <span class="separator-2"></span>' +
-                            '                        <div class="doctor-name">' +
-                            '                            <p class="name">'+doctor_info.Name+'</p>' +
-                            '                            <p class="title">'+doctor_info.Specialization+'</p>' +
-                            '                            <p class="designation">'+doctor_info.ProfessionDegree+'</p>' +
-                            '                        </div>' +
-                            '                        <span class="separator-2"></span>' +
-                            '                        <div class="doctor-address">' +
-                            '                            <span class="icon float-left"><i class="fas fa-map-marker-alt"></i></span>' +
-                            '                            <address class="float-right">' +
-                            '                                Chamber<br>'+doctor_info.ChamberAddress+hotline_info +
-                            '                            </address>' +
-                            '                            <div class="clearfix"></div>' +
-                            '                        </div>' +
-                            '                        <span class="separator-2"></span>' +
-                            '                        <div class="doctor-phone">' +
-                            '                            <span class="icon float-left"><i class="fas fa-mobile"></i></span>' +
-                            '                            <div class="float-right">' +
-                            '                                <h4 class="title">Phone Number</h4>' +
-                            '                                <p class="number">'+doctor_info.PhoneNo+'</p>' +
-                            '                                <p class="number">'+doctor_info.MobileNo1+'</p>' +
-                            '                                <p class="number">'+doctor_info.MobileNo2+'</p>' +
-                            '                                <p class="number">'+doctor_info.MobileNo3+'</p>' +
-                            '                            </div>' +
-                            '                            <div class="clearfix"></div>' +
-                            '                        </div>' +
-                            '                    </div>';
+                        var doctor_li_text = '<div class="doctor-list row">' +
+                            '<div class="doctor-photo col-md-2">' +
+                            '<a href=""><img src="'+image_path+'" alt="doctor"></a>' +
+                            '</div>' +
+                            '<div class="doctor-name  col-md-3">' +
+                            '<h5 class="name">'+doctor_info.Name+'</h5>' +
+                            '<p class="title">'+doctor_info.Specialization+'</p>' +
+                            '<p class="designation">'+doctor_info.ProfessionDegree+'</p>' +
+                            '</div>' +
+                            '<div class="doctor-address  col-md-4">' +
+                            '<span class="icon float-left"><i class="fas fa-map-marker-alt"></i></span> ' +
+                            '<strong>Chamber</strong><br><br>' +
+                            '<address CLASS="clearfix">' +
+                            doctor_info.ChamberAddress+hotline_info +
+                            '</address>' +
+                            '<div class="clearfix"></div>' +
+                            '</div>' +
+                            '<div class="doctor-phone  col-md-2">' +
+                            '<span class="icon float-left"><i class="fas fa-mobile"></i></span> ' +
+                            '<strong>Phone No.</strong><br><br>' +
+                            '<div class="float-right">' +
+                            '<p class="number">'+doctor_info.PhoneNo+'</p>' +
+                            '<p class="number">'+doctor_info.MobileNo1+'</p>' +
+                            '<p class="number">'+doctor_info.MobileNo2+'</p>' +
+                            '<p class="number">'+doctor_info.MobileNo3+'</p>' +
+                            '</div>' +
+                            '<div class="clearfix"></div>' +
+                            '</div>' +
+                            '</div>';
                         $('#doctor-information').append(doctor_li_text);
                     }
                 }
