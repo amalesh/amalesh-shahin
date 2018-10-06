fe6e593fe8bdcc84db20e0519f5bfb5e841f7ea9
Amalesh <amalesh.debnath@gmail.com>
2018-10-06 11:30:36 +0600

    Reason: add limit on auto complete search result

--- application/views/js/frontend-drug-script.php
+++ application/views/js/frontend-drug-script.php
@@ -244,8 +244,12 @@
                 default:
                     break;
             }
+
             $('#searchDrugOption').autocomplete({
-                source: search_options
+                source: function(request, response) {
+                    var results = $.ui.autocomplete.filter(search_options, request.term);
+                    response(results.slice(0, 10));
+                }
             });
         },
         getDrugList: function(getDrugList) {
